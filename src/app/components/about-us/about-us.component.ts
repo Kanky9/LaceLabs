34fcb1e812fb0c9ce2b294503bf64e4505af2851
Kanky9 <bournotsantino03@gmail.com>
2025-09-03 12:57:49 -0300

    new changes

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -12,14 +12,14 @@ export class AboutUsComponent {
   profiles = [
     {
       name: 'Joaquín Bello',
-      role: 'Diseñador UI/UX · Frontend Developer',
+      role: 'Diseñador UI/UX · Frontend Developer · Soporte IT',
       img: '../../../assets/img/about-us/joaco.jpeg',
       cv: '../../../pdf/CVJoaBello.pdf',
       desc: 'Diseño y adapto interfaces en Figma a experiencias visuales modernas y funcionales en Angular e Ionic.'
     },
     {
       name: 'Santino Bournot',
-      role: 'Frontend Developer · Firebase Integration',
+      role: 'Frontend Developer · Firebase Integration · Soporte IT',
       img: '../../../assets/img/about-us/kanky.jpg',
       cv: '../../../pdf/CV-Bournot-Santino.pdf',
       desc: 'Desarrollo aplicaciones web y móviles con Angular/Ionic integradas a Firebase, uniendo diseño, lógica y datos en un solo flujo.'
